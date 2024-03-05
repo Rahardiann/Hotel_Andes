49c45df2f906d803d2e39632f915bd1267eb7999
Sultan Javier <sultan.javier@gmail.com>
2024-03-05 15:02:21 -0800

    ress

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -33,25 +33,9 @@ export default class Header extends React.Component {
             <header className="header bg-white shadow py-4 px-4">
                 <div className="header-content flex items-center flex-row">
                     <form action="#">
-                        <div className="hidden md:flex relative">
+                        <div className=" md:flex relative ml-auto">
                             <h1 className="font-bold text-xl md:text-2xl text-gray-700">Dashboard</h1>
                         </div> 
-                        
-                        <div className="flex md:hidden">
-                            <a href="#" className="flex items-center justify-center h-10 w-10 border-transparent">
-                                <svg
-                                    className="h-6 w-6 text-gray-500"
-                                    fill="none"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                >
-                                    <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                                </svg>
-                            </a>
-                        </div>
                     </form>
                     <div className="flex ml-auto">
                         <a href className="flex flex-row items-center">
@@ -62,12 +46,12 @@ export default class Header extends React.Component {
                             />
                             <span className="flex flex-col ml-2">
                                 <span className="truncate w-20 font-semibold tracking-wide leading-none">{this.state.nama_user}</span>
-                                <span className="truncate w-20 text-gray-500 text-xs leading-none mt-1">{this.state.role}</span>
+                                <span className="truncate w-20 text-gray-500 text-base md:text-xs leading-none mt-1">{this.state.role}</span>
                             </span>
                         </a>
                     </div>
                 </div>
             </header>
-        )
+        );
     }
 }
